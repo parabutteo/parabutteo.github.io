0a7d309c5e432470b0d9310b364e90326c1ab668
Конюшков Николай Константинович <konyushkov.nk@esphere.ru>
2024-12-21 12:19:45 +0300

    feat: add about me info

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,7 +8,19 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Текст писать тут
+          Привет!
+        </p>
+        <p>
+          Меня зовут Николай, я разработчик интерфейсов в компании СберКорус.
+        </p>
+        <p>
+          Пишу на TypeScript, пользуюсь React.
+        </p>
+        <p>
+          Знаком с JS, поверхностно знаю Angular. Есть опыт настройки Webpack и работы c Sass.
+        </p>
+        <p>
+          Хочу углубить познания в ReactJS для написания более осмысленного и функционального кода.
         </p>
       </header>
     </div>
