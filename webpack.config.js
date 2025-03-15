daae130ee55a5ed9200ac4a1d7bef41f535817b0
Конюшков Николай Константинович <konyushkov.nk@esphere.ru>
2025-03-16 00:49:14 +0300

    fix: change publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -31,10 +31,7 @@ module.exports = (_, args) => {
     entry: './index.tsx',
     output: {
       path: dist,
-      publicPath:
-        args.mode === 'development'
-          ? `http://${host}:${port}/`
-          : '/parabutteo.github.io/' /* <- прописать данные своего github */,
+      publicPath: args.mode === 'development' ? `http://${host}:${port}/` : '/' /* <- прописать данные своего github */,
       filename: `js/[name].js`,
       chunkFilename: `js/[name].js`,
     },
