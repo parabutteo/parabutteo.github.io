08143ee17f1e692858a714649fdb3dd3d5537ad5
Конюшков Николай Константинович <konyushkov.nk@esphere.ru>
2025-03-15 23:53:34 +0300

    feat: create profile page

--- src/features/forms/profileForm/ProfileForm.tsx
+++ src/features/forms/profileForm/ProfileForm.tsx
@@ -2,10 +2,14 @@ import { useForm } from 'react-hook-form';
 import React from 'react';
 import { Button } from '../../../components';
 import clsx from 'clsx';
+import { ProfileT } from '../../../pages/Profile';
 
 interface IProfileForm {
   /** Опциональные классы */
   className?: string;
+  form: ProfileT;
+  setForm: React.Dispatch<React.SetStateAction<ProfileT>>;
+  sendHandler: () => void;
 }
 
 type TProfileFormData = {
@@ -17,11 +21,14 @@ type TProfileFormData = {
  * Компонент формы в профиле
  *
  * @param className опциональные css классы
+ * @param form стейт формы
+ * @param setForm диспатч формы
+ * @param sendHandler обработчик отправки формы
  *
  * @returns React.FC
  */
 
-export const ProfileForm: React.FC<IProfileForm> = ({ className }) => {
+export const ProfileForm: React.FC<IProfileForm> = ({ className, form, setForm, sendHandler }) => {
   const {
     register,
     handleSubmit,
@@ -31,6 +38,7 @@ export const ProfileForm: React.FC<IProfileForm> = ({ className }) => {
 
   const onSubmit = (data: TProfileFormData) => {
     console.log('Введенные данные в форме профиля: ', data);
+    sendHandler();
     reset();
   };
 
@@ -40,9 +48,10 @@ export const ProfileForm: React.FC<IProfileForm> = ({ className }) => {
       <div className="grid-content">
         <input
           {...register('name', {
+            value: form.name,
             required: 'Пожалуйста, введите ваше имя',
             pattern: {
-              value: /^[А-Яа-я]+$/g,
+              value: /^[А-Яа-я\s]+$/g,
               message: 'Недопустимые символы в имени',
             },
           })}
@@ -50,17 +59,21 @@ export const ProfileForm: React.FC<IProfileForm> = ({ className }) => {
           type="text"
           id="name"
           placeholder="Введите имя"
+          onChange={(ev) => setForm({ ...form, name: ev.target.value })}
         />
         {errors.name && <p className="error">{errors.name.message}</p>}
       </div>
 
       <label htmlFor="aboutMe">Обо мне</label>
       <textarea
-        {...register('aboutMe')}
+        {...register('aboutMe', {
+          value: String(form.aboutMe),
+        })}
         id="aboutMe"
         className="grid-content"
         rows={4}
         placeholder="Напишите пару слов о себе"
+        onChange={(ev) => setForm({ ...form, aboutMe: ev.target.value })}
       />
 
       <Button className="small" type="submit">
---
+++ src/pages/index.ts
@@ -0,0 +1,9 @@
+export { Profile } from './Profile';
+export { Basket } from './Basket';
+export { Catalog } from './Catalog';
+
+export { Auth } from './Auth';
+
+export { NotFoundPage } from './NotFoundPage';
+
+export { Magic } from './Magic';
