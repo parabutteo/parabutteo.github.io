45da4d97c2c1a9718c29ebef577291346d5b562c
Конюшков Николай Константинович <konyushkov.nk@esphere.ru>
2025-03-21 22:49:58 +0300

    feat: fix const name after review

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../app/styles/common.scss';
 import { ContextProvider } from './ContextProvider';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { NotFoundPage } from '../pages';
 import { routes } from '../pages/routes';
 import { AccessDenied } from 'src/pages/AccessDenied';
@@ -14,21 +14,19 @@ import { AccessDenied } from 'src/pages/AccessDenied';
 
 export const App: React.FC = () => {
   const urlParams = new URLSearchParams(window.location.search);
-  const isAuth = urlParams.has('user') && urlParams.get('user') === 'auth';
+  const userIsAuth = urlParams.has('user') && urlParams.get('user') === 'auth';
 
   return (
     <ContextProvider>
-      <BrowserRouter basename="/">
-        <Routes>
-          {routes.map((route) => {
-            if (route.isAuth && !isAuth) {
-              return <Route key="access-denied" path="magic" element={<AccessDenied />} />;
-            }
-            return <Route key={route.path} path={route.path} element={<route.component />} />;
-          })}
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        {routes.map((route) => {
+          if (route.isAuth && !userIsAuth) {
+            return <Route key="access-denied" path="magic" element={<AccessDenied />} />;
+          }
+          return <Route key={route.path} path={route.path} element={<route.component />} />;
+        })}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </ContextProvider>
   );
 };
