359c32f9f74ae4bafad9827d84707951a49ae2d3
Конюшков Николай Константинович <konyushkov.nk@esphere.ru>
2025-03-28 22:22:05 +0300

    feat: add store and hooks

---
+++ src/store/store.ts
@@ -0,0 +1,19 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../features/auth/authSlice';
+// import profileReducer from './profileSlice';
+// import cartReducer from './cartSlice';
+// import productsReducer from './productsSlice';
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    // profile: profileReducer,
+    // cart: cartReducer,
+    // products: productsReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
