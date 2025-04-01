eb89917c23ae5774528f65520c94816a2db42c6c
Конюшков Николай Константинович <konyushkov.nk@esphere.ru>
2025-04-01 23:34:03 +0300

    feat: add localstorage to cart

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -2,35 +2,50 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface CartState {
   items: {
-    id: number;
+    id: string;
     quantity: number;
-    price: number;
-    title: string;
   }[];
 }
 
-const initialState: CartState = {
-  items: [],
+const saveStateToLocalStorage = (state: CartState) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('cart', serializedState);
+  } catch (error) {
+    console.error('Ошибка сохранения состояния в localStorage:', error);
+  }
 };
+
+const loadCartStateFromLocalStorage = (): CartState => {
+  try {
+    const serializedState = localStorage.getItem('cart');
+    if (!serializedState) return { items: [] };
+    return JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Ошибка загрузки состояния из localStorage:', error);
+    return { items: [] };
+  }
+};
+
+const initialState: CartState = loadCartStateFromLocalStorage();
 
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-    addItemToCart(state, action: PayloadAction<{ id: number; title: string; price: number }>) {
+    addItemToCart(state, action: PayloadAction<{ id: string }>) {
       const existingItem = state.items.find((item) => item.id === action.payload.id);
       if (existingItem) {
         existingItem.quantity += 1;
       } else {
         state.items.push({
           id: action.payload.id,
-          title: action.payload.title,
           quantity: 1,
-          price: action.payload.price,
         });
       }
+      saveStateToLocalStorage(state);
     },
-    removeItemFromCart(state, action: PayloadAction<number>) {
+    removeItemFromCart(state, action: PayloadAction<string>) {
       const existingItem = state.items.find((item) => item.id === action.payload);
       if (existingItem) {
         existingItem.quantity -= 1;
@@ -38,12 +53,18 @@ const cartSlice = createSlice({
           state.items = state.items.filter((item) => item.id !== action.payload);
         }
       }
+      saveStateToLocalStorage(state);
     },
-    removeFromCart(state, action: PayloadAction<number>) {
+    removeFromCart(state, action: PayloadAction<string>) {
       state.items = state.items.filter((item) => item.id !== action.payload);
+      saveStateToLocalStorage(state);
+    },
+    clearCart(state) {
+      state.items = [];
+      saveStateToLocalStorage(state);
     },
   },
 });
 
-export const { addItemToCart, removeItemFromCart, removeFromCart } = cartSlice.actions;
+export const { addItemToCart, removeItemFromCart, removeFromCart, clearCart } = cartSlice.actions;
 export default cartSlice.reducer;
