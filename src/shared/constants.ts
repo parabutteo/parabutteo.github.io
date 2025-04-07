2c31341135ad051ba48f9c1d56f28fe147638a1e
Конюшков Николай Константинович <konyushkov.nk@esphere.ru>
2025-04-07 23:43:12 +0300

    feat: add files with constants

---
+++ src/shared/constants.ts
@@ -0,0 +1,7 @@
+export const REST_API_SERVER_URL = 'https://19429ba06ff2.vps.myjino.ru/api';
+export const COMMAND_ID = 'parabutteo';
+
+export const backendErrorMessages: Record<string, string> = {
+  ERR_VALIDATION_ERROR: 'Введите корректный адрес электронной почты',
+  ERR_ACCOUNT_ALREADY_EXIST: 'Уже существует пользователь с данным адресом электронной почты',
+};
